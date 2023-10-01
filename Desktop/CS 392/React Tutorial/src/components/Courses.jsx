bf6d2eee9ff0eadc2000831637c02c8e7ada2f23
cmorcott16 <cmorcott@gmail.com>
2023-10-01 17:25:20 -0500

    Added course selection

--- Desktop/CS 392/React Tutorial/src/components/Courses.jsx
+++ Desktop/CS 392/React Tutorial/src/components/Courses.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+// import React from 'react';
+// import { useState } from "react";
+// import './Courses.css';
+
+// const Courses = ({ courseList }) => {
+//     return (
+//         <div className="courseListContainer">
+//             <div className="courseList">
+//                 {Object.entries(courseList).map(([key, value]) => (
+//                     <div className='card m-1 p-2' key={key}>
+//                         <div className='card-body'>
+//                             <h5 className='card-title'>{value.term} CS {value.number}</h5>
+//                             <p className='card-text'>Term: {value.title}</p>
+//                         </div>
+//                         <div className='card-footer bg-white'>
+//                             <p className='card-text'>Meets: {value.meets}</p>
+//                         </div>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Courses;
+
+
+import React, { useState } from 'react';
 import './Courses.css';
 
 const Courses = ({ courseList }) => {
+    const [selected, setSelected] = useState([]);
+
+    const toggleSelected = (key) => {
+        console.log("Toggling selection for:", key)
+        setSelected(
+            selected.includes(key)
+            ? selected.filter(x => x !== key)
+            : [...selected, key]
+        );
+    }
+
     return (
         <div className="courseListContainer">
             <div className="courseList">
                 {Object.entries(courseList).map(([key, value]) => (
-                    <div className='card m-1 p-2' key={key}>
+                    <div 
+                        className={`card m-1 p-2 ${selected.includes(key) ? 'selectedCourse' : ''}`} 
+                        key={key} 
+                        onClick={() => toggleSelected(key)}
+                        style={selected.includes(key) ? {border: '2px solid red'} : {}}
+                    >
                         <div className='card-body'>
                             <h5 className='card-title'>{value.term} CS {value.number}</h5>
                             <p className='card-text'>Term: {value.title}</p>
@@ -22,3 +65,4 @@ const Courses = ({ courseList }) => {
 }
 
 export default Courses;
+
--- Desktop/CS 392/React Tutorial/src/components/TermButtons.jsx
+++ Desktop/CS 392/React Tutorial/src/components/TermButtons.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import './TermButtons.css';
 
 const terms = {
